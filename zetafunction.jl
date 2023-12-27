2c75085fc4c09d338e53e2ec7a9df4676f0f02c8
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2023-12-27 03:08:46 -0500

    Julia plots

---
+++ zetafunction.jl
@@ -0,0 +1,26 @@
+using SpecialFunctions
+using Plots
+
+function convert_to_imaginary(arr)
+    return [(0.5 + x * im) for x in arr]
+end
+
+
+
+
+
+z = collect(-10:0.01:20)
+imag_z = convert_to_imaginary(z)
+spectrum = zeta.(imag_z)
+x_val = real(spectrum)
+y_val = imag(spectrum)
+
+theme(:dracula)
+
+p = plot(x_val,y_val,legend=false,framestyle=:origin,color="cyan")
+
+
+savefig(p,"static/zetamodified.png")
+
+
+
